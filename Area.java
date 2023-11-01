4d5112631e355e3e3ec665dd6f7aaf0b2040fdda
Atla Ramya <98703453+21P31A04D5@users.noreply.github.com>
2023-11-01 14:28:59 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,14 @@
+
+public class Area {
+    public static void main(String[] args) {
+        int r=2;
+
+        final double PI = 3.141592653589793238462643;
+         
+        double area = PI * (r)*(r);
+        double perimeter = 2* PI * r;
+
+        System.out.println("Perimeter is = " +perimeter);
+        System.out.println("Area is = " +area);
+        }
+    }
